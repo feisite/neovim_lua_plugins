2d3cf9366aa552ef9437d6e086be7b252372c714
RxmoneySSS <mengdesijiu@outlook.com>
2023-04-02 22:14:10 +0800

    delete bug
    delete bug

--- nvim/lua/treesitter/treesitter.lua
+++ nvim/lua/treesitter/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "c", "lua", "vim", "help", "query" },
+  ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
@@ -54,3 +54,4 @@ require'nvim-treesitter.configs'.setup {
     }
   },
 }
+
